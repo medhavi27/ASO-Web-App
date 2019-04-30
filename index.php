0896aa9c5b0720cb2d000b1a8337f70ec479127c
Medhavi Gandhi <mg2252@cornell.edu>
2019-04-29 21:30:54 -0400

    Adding meesage from prez

--- index.php
+++ index.php
@@ -35,10 +35,11 @@ include("includes/init.php");
     Armenia. Finally, we seek to promote intersectionality and build connections
     with other student groups on campus.
   </p>
-
   <hr />
-
   <h2 class="pres_text">A Message From Our President</h2>
+  <p class="message">
+    P/Barev and welcome to the Armenian Student Organization at Cornell University, your Big Red (Blue, and Orange) home away from տուն! I first became involved in the ASO on campus as a first-year undergraduate student, and quickly found comfort in this small, yet welcoming Cornellian Armenian community over the years, and I hope you will too! Join us for monthly meetings over Armenian coffee, BBQs at Stewart Park, weekly Armenian language and culture lessons, outings to NYC, and our many other events throughout the semester with fellow undergraduate and graduate students, professors and faculty, and local community members. We are a cultural student organization, with a focus on curating social, educational, and professional events and connections for the Cornell and Ithaca community at large. The ASO is open to all that are interested in learning more about Armenian culture!
+  </p>
 
   <hr />
 
