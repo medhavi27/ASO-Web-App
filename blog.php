78d0adef32c42e0f29acbaf2ca7f33fbe5d230c0
Joseph <jjm492@cornell.edu>
2019-04-30 18:30:15 -0400

    b-ground img covers, fixed eboard,finished contact

--- blog.php
+++ blog.php
@@ -1,10 +1,10 @@
 <?php
 // INCLUDE ON EVERY TOP-LEVEL PAGE!
-function getsource($inp) {
+function getsource($inp)
+{
   $substr = explode(".", $inp);
   $res = $substr[1];
   return ucfirst($res);
-
 }
 
 
@@ -26,18 +26,22 @@ include("includes/init.php");
 
   <!-- TODO: This should be your main page for your site. -->
   <?php include("includes/header.php") ?>
-  <h2 class="about">Learn more about ASO at Cornell and Armenia</h2>
-  <hr>
+
+  <div class="blog_background">
+    <h2 class="background_text">Learn More</h2>
+  </div>
+
+  <!-- <h2 class="about">Learn more about ASO at Cornell and Armenia</h2>
+  <hr> -->
   <div class="row">
     <?php $records = exec_sql_query($db, "SELECT * FROM blogs")->fetchAll(PDO::FETCH_ASSOC);
     foreach ($records as $record) {
 
-      echo  "<div class='post'><h3 class='posth3'><a href='".htmlspecialchars($record['link']) ."'>". htmlspecialchars($record["title"]) . "</a></h3>";
-      echo "<ul><li> Source: <a href='".htmlspecialchars($record['link'])."'>" . htmlspecialchars(getsource($record["link"])) . "</a></li>
-      <li> Author: ".htmlspecialchars($record["author"])."</li>
-      <li> Date: ".htmlspecialchars($record["date"])."</li>
+      echo  "<div class='post'><h3 class='posth3'><a href='" . htmlspecialchars($record['link']) . "'>" . htmlspecialchars($record["title"]) . "</a></h3>";
+      echo "<ul><li> Source: <a href='" . htmlspecialchars($record['link']) . "'>" . htmlspecialchars(getsource($record["link"])) . "</a></li>
+      <li> Author: " . htmlspecialchars($record["author"]) . "</li>
+      <li> Date: " . htmlspecialchars($record["date"]) . "</li>
       <ul></div>";
-
     } ?>
 
   </div>
